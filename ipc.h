43f7a42dd00695669fc6b4ddc9795c0810ab939d
avstiix <lunamartinez55@outlook.com>
2024-12-23 11:45:23 -0500

    Update ipc.h to include necessary headers and modify send_message function signature

--- ipc.h
+++ ipc.h
@@ -1,3 +1,6 @@
+#include <stdint.h>
+#include <sys/types.h>
+
 struct message {
     uint32_t sender;
     uint32_t receiver;
@@ -5,5 +8,5 @@ struct message {
     char data[256];
 };
 
-int send_message(pid_t dest, struct message* msg);
+int send_message(uint32_t dest, struct message* msg);
 int receive_message(struct message* msg);
