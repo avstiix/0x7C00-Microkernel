80930b7b357a391ca3b168c5c618f7073cfd6152
avstiix <lunamartinez55@outlook.com>
2024-12-23 11:45:44 -0500

    Enhance mmu.h by including stdint.h and updating map_page function signature for clarity

--- mmu.h
+++ mmu.h
@@ -1,6 +1,8 @@
+#include <stdint.h>
+
 struct page_table {
     uint32_t entries[1024];
 };
 
 void init_paging();
-void map_page(void* physical, void* virtual, uint32_t flags);
+void map_page(void* physical, void* virtual_addr, uint32_t flags);
